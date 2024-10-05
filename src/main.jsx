770a3300ae337244b02df8f6a634101d9947db78
GDiamend <GDiamend@gmail.com>
2024-10-05 20:02:11 -0300

    Se elimina el .env y se agregan las keys al main para poder levantar el proyecto desde cualquier lado

--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,23 @@ import 'animate.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import { initializeApp } from "firebase/app";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: "AIzaSyCK4amdEnmDD7AjlC1reUwtSHu9CAEtZVk",
+  authDomain: "proyecto-final-react-60025.firebaseapp.com",
+  projectId: "proyecto-final-react-60025",
+  storageBucket: "proyecto-final-react-60025.appspot.com",
+  messagingSenderId: "994006167344",
+  appId: "1:994006167344:web:d5041129bb8cc382cd62f5"
 };
 
-export default firebaseConfig;
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 createRoot(document.getElementById('root')).render(
