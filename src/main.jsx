8c4bd829d1463d8e282e9ff9cf2243f6e5425f3b
GDiamend <GDiamend@gmail.com>
2024-10-29 16:02:22 -0300

    Se vuelven a ocultar las claves en el .env

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,15 @@ import { BrowserRouter } from "react-router-dom";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK4amdEnmDD7AjlC1reUwtSHu9CAEtZVk",
-  authDomain: "proyecto-final-react-60025.firebaseapp.com",
-  projectId: "proyecto-final-react-60025",
-  storageBucket: "proyecto-final-react-60025.appspot.com",
-  messagingSenderId: "994006167344",
-  appId: "1:994006167344:web:d5041129bb8cc382cd62f5"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+
+export default firebaseConfig;
 
 const app = initializeApp(firebaseConfig);
 
